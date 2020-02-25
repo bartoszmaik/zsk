1919af363eca7a08c8fab183cdecce72e7703457
bartoszmaik <54834590+bartoszmaik@users.noreply.github.com>
2020-02-25 08:44:49 +0100

    Add files via upload

---
+++ uprawnienia/scripts/add_user.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+  if (isset($_POST['button']) && !empty($_POST['name']) && !empty($_POST['surname']) && !empty($_POST['birthday'])) {
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $birthday = $_POST['birthday'];
+
+    require_once('./connect.php');
+
+    $sql = "INSERT INTO `user`( `name`, `surname`, `birthday`) VALUES ('$name', '$surname', '$birthday')";
+
+    mysqli_query($conn, $sql);
+  }else $_SESSION['error'] = "Wypełnij wszystkie pola";
+  header('location: ../');
+ ?>
